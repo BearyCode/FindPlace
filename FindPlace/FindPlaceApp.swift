3dbb8bf783145bcf5acf36cffd5b63cdac2a1eed
BearyCode <rettingerm@protonmail.com>
2024-09-24 10:31:43 +0200

    Initial Commit

---
+++ FindPlace/FindPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindPlaceApp.swift
+//  FindPlace
+//
+//  Created by BearyCode on 24.09.24.
+//
+
+import SwiftUI
+
+@main
+struct FindPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
